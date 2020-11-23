2c120011f73f4c15699c31569a33e8643316697f
Ashishnoobdev <52867775+Ashishnoobdev@users.noreply.github.com>
2020-11-24 01:48:58 +0530

    Started with card component recieving dynamic values

---
+++ src/CardContentStructure.js
@@ -0,0 +1,44 @@
+export const cardContentStructure = [
+    {
+        id: 1,
+        name: 'xoxo',
+        description: 'bjkbfskjn',
+        price: '100$'
+    },
+    {
+        id: 2,
+        name: 'zozo',
+        description: 'bjkbfskjn',
+        price: '50$'
+    },
+    {
+        id: 3,
+        name: 'zozo',
+        description: 'bjkbfskjn',
+        price: '120$'
+    },
+    {
+        id: 4,
+        name: 'zozo',
+        description: 'bjkbfskjn',
+        price: '150$'
+    },
+    {
+        id: 5,
+        name: 'zozo',
+        description: 'bjkbfskjn',
+        price: '160$'
+    },
+    {
+        id: 6,
+        name: 'zozo',
+        description: 'bjkbfskjn',
+        price: '170$'
+    },
+    {
+        id: 7,
+        name: 'MorningStar',
+        description: 'Fuck off Nigga!',
+        price: '$$'
+    }
+];
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import { cardContentStructure } from './CardContentStructure';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Virtual Shopping Stop!</h1>
+    <Card id={cardContentStructure[0].id} name={cardContentStructure[0].name} description={cardContentStructure[0].description} price={cardContentStructure[0].price} />
+    <Card id={cardContentStructure[1].id} name={cardContentStructure[1].name} description={cardContentStructure[1].description} price={cardContentStructure[1].price} />
+    <Card id={cardContentStructure[6].id} name={cardContentStructure[6].name} description={cardContentStructure[6].description} price={cardContentStructure[6].price} />
+    <Card id={cardContentStructure[2].id} name={cardContentStructure[2].name} description={cardContentStructure[2].description} price={cardContentStructure[2].price} />
+    <Card id={cardContentStructure[4].id} name={cardContentStructure[4].name} description={cardContentStructure[4].description} price={cardContentStructure[4].price} />
   </React.StrictMode>,
   document.getElementById('root')
 );
