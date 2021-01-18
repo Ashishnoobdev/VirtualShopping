939b1b86b589e9a3ffa8553512d7b662fccfc666
Ashishnoobdev <52867775+Ashishnoobdev@users.noreply.github.com>
2021-01-18 12:20:23 +0530

    Updated name of Robs

--- src/CardContentStructure.js
+++ src/CardContentStructure.js
@@ -1,49 +1,49 @@
 export const cardContentStructure = [
     {
         id: 1,
-        name: 'xoxo',
-        description: 'bjkbfskjn',
+        name: 'Robot One',
+        description: 'This is the description',
         price: '100$'
     },
     {
         id: 2,
-        name: 'zozo',
-        description: 'bjkbfskjn',
+        name: 'Robot Two',
+        description: 'This is the description',
         price: '50$'
     },
     {
         id: 3,
-        name: 'zozo',
-        description: 'bjkbfskjn',
+        name: 'Robot Three',
+        description: 'This is the description',
         price: '120$'
     },
     {
         id: 4,
-        name: 'zozo',
-        description: 'bjkbfskjn',
+        name: 'Robot Four',
+        description: 'This is the description',
         price: '150$'
     },
     {
         id: 5,
-        name: 'zozo',
-        description: 'bjkbfskjn',
+        name: 'Robot Five',
+        description: 'This is the description',
         price: '160$'
     },
     {
         id: 6,
-        name: 'zozo',
-        description: 'bjkbfskjn',
+        name: 'Robot Six',
+        description: 'This is the description',
         price: '170$'
     },
     {
         id: 7,
         name: 'MorningStar',
-        description: 'Fuck off Nigga!',
+        description: 'The First Born',
         price: '$$'
     },
     {
         id: 8,
-        name: 'NewItem',
+        name: 'Robot Again',
         description: 'Be Nice!',
         price: '100$'
     },
